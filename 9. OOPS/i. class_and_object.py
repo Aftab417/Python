2ecc379c225834b14f980df4c77be44632801145
Aftab417 <aftabkhan1567ss@gmail.com>
2025-02-04 14:51:09 +0500

    static methods

--- 9. OOPS/i. class_and_object.py
+++ 9. OOPS/i. class_and_object.py
@@ -1,5 +1,7 @@
 
 
+###################---------- About Class --------------#####################
+
 # 1. Class:
 # A class is a blueprint or template for creating objects.
 # It defines a set of attributes (data) and methods (functions) that the objects created from the class will have.
@@ -25,6 +27,7 @@
 
 
 
+###################---------- About Object --------------#####################
 
 
 # 2. Object:
@@ -63,6 +66,19 @@
 
 
 
+
+
+###################---------- Constructor  __init__() function --------------#####################
+
+
+# All classes have a function called __init__(), which is always executed when the class is being initiated.
+
+# Use the __init__() function to assign values to object properties, 
+# or other operations that are necessary to do when the object is being created:
+
+
+
+
 # class person:
 #     def __init__(self, name, age):
 #         self.name = name
@@ -77,9 +93,9 @@
 
 
 
+###################---------- Constructor  __str__() function --------------#####################
 
-# __str__() :  The __str__() function controls what should be returned when the class object is represented as a string.
-
+#  The __str__() function controls what should be returned when the class object is represented as a string.
 # If the __str__() function is not set, the string representation of the object is returned:
 
 
@@ -103,6 +119,7 @@
 
 
 
+###################---------- Instance Method --------------#####################
 
 # Object Method : Objects can also contain methods. Methods in objects are functions that belong to the object.
 
@@ -129,6 +146,7 @@
 
 
 
+###################---------- Modifing Object Attributes --------------#####################
 
 # Modify object Properties :  We can modigy object properties like this.
 
@@ -155,6 +173,7 @@
 
 
 
+###################---------- Deleting Object Attributes --------------#####################
 
 
 # Deleting Object Properties :  We can delete object properties using "del" keyword.
@@ -182,6 +201,7 @@
 
 
 
+###################---------- Deleting Object  --------------#####################
 
 # Deleting an Object :  We can delete an object like this.
 
@@ -211,20 +231,13 @@
 
 
 
+###################---------- Pass Statement --------------#####################
 
 #  Pass Statement :  We can use "Pass" statement if the class is empty to avoid an error.
 
 # class person:
 #     pass
 
-
-
-
-
-
-
-
- 
 
 
 # class Dog:
@@ -241,12 +254,6 @@
 # print(my_dog.bark())
 # print(my_dog.species)
 # print(my_dog.age)
-
-
-
-
-
-
 
 
 
@@ -277,8 +284,6 @@
 
 
 
-
-
 # class BankAccount:
 #     def __init__(self, owner, balance = 0):
 #         self.owner = owner
@@ -303,6 +308,212 @@
 # account.deposit(10000)
 
 # print(account.get_balance())
+
+# account.withdraw(4000)
+# print(account.get_balance())
+
+
+
+
+
+
+
+#########################------------- Types of Methods --------------###########################
+
+
+# In Python, methods within a class can be categorized into three types based on how they interact with the class and its instances:
+
+# 1. Instance Methods
+# 2. Class Methods
+# 3. Static Methods
+
+# Here’s a detailed comparison of the three:
+
+# 1. Instance Methods
+# Definition: Instance methods are the most common type of methods in a class. 
+# They operate on an instance of the class and have access to the instance's data (attributes) and other instance methods.
+
+# First Argument: The first argument is always self, which refers to the instance of the class.
+
+# Usage: Used to perform operations that involve instance-specific data.
+
+# Calling: Can only be called on an instance of the class.
+
+# Decorator: No decorator is needed (default behavior).
+
+
+
+# Example:
+ 
+# class MyClass:
+#     def instance_method(self):
+#         return f"Called instance_method of {self}"
+
+# obj = MyClass()
+# print(obj.instance_method())  # Output: Called instance_method of <__main__.MyClass object>
+
+
+
+# class student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def greet(self):
+#         print(f"Hi, My name is {self.name}")
+
+# s1 = student("Ali", 20)
+
+# s1.greet()
+
+
+
+
+
+
+
+
+
+
+
+
+# 2. Class Methods
+# Definition: Class methods operate on the class itself rather than on instances. 
+# They can access and modify class-level data (class variables).
+
+# First Argument: The first argument is always cls, which refers to the class (not the instance).
+
+# Usage: Used for factory methods, alternative constructors, or operations that involve class-level data.
+
+# Calling: Can be called on the class itself or on an instance of the class.
+
+# Decorator: Defined using the @classmethod decorator.
+
+
+
+
+# Example:
+ 
+# class MyClass:
+#     class_variable = 10
+
+#     @classmethod
+#     def class_method(cls):
+#         return f"Called class_method of {cls}, class_variable = {cls.class_variable}"
+
+# print(MyClass.class_method())  # Output: Called class_method of <class '__main__.MyClass'>, class_variable = 10
+
+
+
+
+# class Dog:
+#     species = "Canis"
+
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     @classmethod
+#     def dog_type(cls):
+#         print(f"The Type of Dog is {cls.species}")
+
+# Dog.dog_type()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 3. Static Methods
+# Definition: Static methods are independent of both the class and its instances.
+#  They behave like regular functions but are defined within the class's namespace for organizational purposes.
+
+# First Argument: No self or cls argument. They do not have access to instance or class-specific data.
+
+# Usage: Used for utility functions that are logically related to the class but do not depend on instance or class state.
+
+# Calling: Can be called on the class itself or on an instance of the class.
+
+# Decorator: Defined using the @staticmethod decorator.
+
+
+
+
+# Example:
+ 
+# class MyClass:
+#     @staticmethod
+#     def static_method():
+#         return "Called static_method"
+
+# print(MyClass.static_method())  # Output: Called static_method
+
+
+
+
+
+
+
+
+# Summary:
+
+# Instance Methods: Work with instance-specific data (self).
+
+# Class Methods: Work with class-level data (cls).
+
+# Static Methods: Work independently of instance or class data.
+
+# Choosing the right type of method depends on whether the functionality depends on instance data, class data, or neither.
+ 
+
+
+
+
+
+
+
+# Example Combining All Three:
+ 
+# class MyClass:
+#     class_variable = 100
+
+#     def __init__(self, instance_variable):
+#         self.instance_variable = instance_variable
+
+#     # Instance Method
+#     def instance_method(self):
+#         return f"Instance method: instance_variable = {self.instance_variable}"
+
+#     # Class Method
+#     @classmethod
+#     def class_method(cls):
+#         return f"Class method: class_variable = {cls.class_variable}"
+
+#     # Static Method
+#     @staticmethod
+#     def static_method():
+#         return "Static method: No access to instance or class data"
+
+# # Instance Method
+# obj = MyClass(10)
+# print(obj.instance_method())  # Output: Instance method: instance_variable = 10
+
+# # Class Method
+# print(MyClass.class_method())  # Output: Class method: class_variable = 100
+
+# # Static Method
+# print(MyClass.static_method())  # Output: Static method: No access to instance or class data
+
 
 
 
---
+++ Exercises/apnacollage12.py
@@ -0,0 +1,39 @@
+
+
+#  Create a  class named "Student" that takes the name and the marks of three subjects as arguments in constructor. Then write a method to print the average of three subjects
+
+
+# class Student:
+#     def __init__(self, name, m1, m2, m3):
+#         self.name = name
+#         self.m1 = m1
+#         self.m2 = m2
+#         self.m3 = m3
+
+#     def print_average(self):
+#         average = (self.m1 + self.m2 + self.m3) / 3
+#         print(average)
+
+# s1 = Student("Ali", 90, 80, 97)
+
+# s1.print_average()
+
+
+
+
+# Alternative:
+
+# class student:
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+
+#     def get_avr_marks(self):
+#         return sum(val for val in self.marks)/len(self.marks)
+    
+# s1 = student("Aftab", [98, 96, 90])
+
+# print(s1.get_avr_marks())
+
+
+
