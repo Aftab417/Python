cd141e7f91c842d662932d7db75da36e20951dd3
Aftab417 <aftabkhan1567ss@gmail.com>
2025-02-24 09:41:13 +0500

    File arrangement

--- 4. Data Types in Python/typecasting.py
+++ 4. Data Types in Python/typecasting.py
@@ -1,11 +1,11 @@
-x = 1.2
-y = 2
-z = "alrighttech"
+# x = 1.2
+# y = 2
+# z = "alrighttech"
 
-a = int(x)
-b = float(y)
-c = str(z)
+# a = int(x)
+# b = float(y)
+# c = str(z)
 
-print(a, b, c)
-a = int(3)
-print(type(a))
+# print(a, b, c)
+# a = int(3)
+# print(type(a))
